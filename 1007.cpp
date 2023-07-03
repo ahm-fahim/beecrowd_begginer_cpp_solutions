bbec16e350839b7855b82b594598717ddddb9b47
ahm-fahim <contactahmfahim@gmail.com>
2023-07-03 13:19:39 +0600

    1007 solved

---
+++ 1007.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+    int d;
+
+    cin >> a>> b >> c >> d;
+    int differance = a*b - c*d;
+
+    cout << "DIFERENCA = "<<differance <<endl;
+
+    return 0;
+}

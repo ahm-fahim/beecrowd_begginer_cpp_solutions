352b6c44805d0d9a892453ca51f94c35598103fe
ahm-fahim <contactahmfahim@gmail.com>
2023-07-03 13:02:45 +0600

    1005 solved

---
+++ 1005.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double A;
+    double B;
+    double MEDIA;
+
+    cin >> A >> B;
+
+    MEDIA = ((A*3.5) + (B*7.5))/(3.5+7.5);
+
+    cout << fixed << setprecision(5);
+    cout << "MEDIA = " << MEDIA << endl;
+
+    return 0;
+}

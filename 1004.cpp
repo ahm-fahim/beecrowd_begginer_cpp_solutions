779dca74fd51199bd3a977f23a9a76f2ee4ddbbf
ahm-fahim <contactahmfahim@gmail.com>
2023-07-03 12:35:48 +0600

    1004

---
+++ 1004.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int PROD;
+
+    cin >> a >> b;
+
+    PROD = a*b;
+
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}

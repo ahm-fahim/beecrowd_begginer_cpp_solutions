753cf8814e8e5f12f5e7b3a298ae8d4aa86c958c
ahm-fahim <contactahmfahim@gmail.com>
2023-07-03 16:18:14 +0600

    1013 solved

---
+++ 1013.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b >> c;
+
+    if(a >= b && a >= c ){
+        cout << a << " eh o maior"<< endl;
+    }else if(b >= c && b >= a ){
+        cout << b << " eh o maior"<< endl;
+    }else{
+        cout << c << " eh o maior"<< endl;
+    }
+
+    return 0;
+}
